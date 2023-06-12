c9072f0504963cf3d2eb090e301a43aa45f4b0d8
Joshua Cavell <xclusive36@gmail.com>
2023-06-12 10:00:13 -0400

    Update database.js to refine putDB and getDB functions

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,45 +1,37 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 export const putDb = async (content) => {
-  // add the content to the database
-  // return the content
-  openDB('jate', 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
-      }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    }
-  });
+  // update the database with the new content
+  const db = await openDB("jate", 1);
+  const tx = db.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  await store.put(content);
+  await tx.done;
+  console.log("jate database updated");
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  // get all the content from the database
-  // return the content
-  openDB('jate', 1, {
+  // if the database is empty, create a new one and return it empty otherwise return the content
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
+      if (db.objectStoreNames.contains("jate")) { // check if the database exists
+        return; // if it does, return it
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    }
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true }); // if it doesn't, create it
+    },
   });
 };
 

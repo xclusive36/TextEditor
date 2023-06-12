e89758c8d4835aef59e3c0ceabf51e49ee77ff8b
Joshua Cavell <xclusive36@gmail.com>
2023-06-12 09:46:01 -0400

    Update database.js
    Update database.js putDB and getDB functions

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,35 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  // add the content to the database
+  // return the content
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    }
+  });
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  // get all the content from the database
+  // return the content
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    }
+  });
+};
 
 initdb();

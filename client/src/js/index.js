cb8acf11eed364d27297aa12d6b5cff07a17acf6
Joshua Cavell <xclusive36@gmail.com>
2023-06-12 12:05:43 -0400

    Update files with readable comments

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,33 +1,31 @@
-import { Workbox } from 'workbox-window';
-import Editor from './editor';
-import './database';
-import '../css/style.css';
+import { Workbox } from 'workbox-window'; // import workbox window
+import Editor from './editor'; // import editor class
+import './database'; // import database
+import '../css/style.css'; // import css
 
-const main = document.querySelector('#main');
-main.innerHTML = '';
+const main = document.querySelector('#main'); // get main element from DOM
+main.innerHTML = ''; // clear main element
 
-const loadSpinner = () => {
-  const spinner = document.createElement('div');
-  spinner.classList.add('spinner');
+const loadSpinner = () => { // create spinner element and append to main element
+  const spinner = document.createElement('div'); // create spinner element as a div
+  spinner.classList.add('spinner'); // add spinner class to spinner element
   spinner.innerHTML = `
   <div class="loading-container">
   <div class="loading-spinner" />
   </div>
   `;
-  main.appendChild(spinner);
+  main.appendChild(spinner); // append spinner element to main element
 };
 
-const editor = new Editor();
+const editor = new Editor(); // create new editor instance
 
-if (typeof editor === 'undefined') {
-  loadSpinner();
+if (typeof editor === 'undefined') { // check if editor is undefined
+  loadSpinner(); // load spinner
 }
 
-// Check if service workers are supported
-if ('serviceWorker' in navigator) {
-  // register workbox service worker
-  const workboxSW = new Workbox('/src-sw.js');
-  workboxSW.register();
-} else {
-  console.error('Service workers are not supported in this browser.');
+if ('serviceWorker' in navigator) { // check if service worker is supported
+  const workboxSW = new Workbox('/src-sw.js'); // create new workbox service worker
+  workboxSW.register(); // register service worker
+} else { // if service worker is not supported
+  console.error('Service workers are not supported in this browser.'); // log error
 }

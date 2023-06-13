6fb543658b828ee9346cbf23bb781847cc013904
Joshua Cavell <xclusive36@gmail.com>
2023-06-12 20:46:53 -0400

    IndexedDB properly sets

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,9 +1,13 @@
 import { openDB } from "idb"; // import the idb library
 
-const initdb = async () => // create a function that initializes the database
-  openDB("jate", 1, { // open the database
-    upgrade(db) { // upgrade the database
-      if (db.objectStoreNames.contains("jate")) { // check if the database exists
+const initdb = async () =>
+  // create a function that initializes the database
+  openDB("jate", 1, {
+    // open the database
+    upgrade(db) {
+      // upgrade the database
+      if (db.objectStoreNames.contains("jate")) {
+        // check if the database exists
         console.log("jate database already exists"); // if it does, return it and log it
         return;
       }
@@ -12,22 +16,23 @@ const initdb = async () => // create a function that initializes the database
     },
   });
 
-export const putDb = async (content) => { // create a function that updates the database
-  const db = await openDB("jate", 1); // open the database
-  const tx = db.transaction("jate", "readwrite"); // create a transaction to update the database
-  const store = tx.objectStore("jate"); // create a store to update the database
-  await store.put(content); // update the database
-  await tx.done; // close the transaction
-  console.log("jate database updated"); // log the update of the database
+export const putDb = async (content) => { // update the database
+  const contactDb = await openDB('jate', 1); // open the database
+  const tx = contactDb.transaction('jate', 'readwrite'); // create a transaction
+  const store = tx.objectStore('jate'); // create a store
+  const request = store.add({content: content}); // add the content to the store
+  const result = await request; // wait for the request to finish
 };
 
-export const getDb = async () => { // create a function that gets the database
+export const getDb = async () => { // grab data from the database
   openDB("jate", 1, { // open the database
-    upgrade(db) { // upgrade the database
+    upgrade(db) { // upgrade the database if it doesn't exist
       if (db.objectStoreNames.contains("jate")) { // check if the database exists
-        return; // if it does, return it
+        console.log("jate database already exists"); // if it does, return it and log it
+        return; // return the database
       }
-      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true }); // if it doesn't, create it
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true }); // if it doesn't, create it and log it
+      console.log("jate database created"); // log the creation of the database
     },
   });
 };
